91f1dc4b0c59c06afcb3c0530addd1dd0fd3ad68
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-10-26 14:36:57 +0300

    added some products

--- components/Product.jsx
+++ components/Product.jsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import React from "react";
 
 export default function Product({ product }) {
+  const discount =
+    ((product.originalPrice - product.currentPrice) / product.originalPrice) *
+    100;
   return (
     <div className="relative flex items-center flex-col justify-center border shadow rounded-sm">
       <small className="absolute left-2 top-2 font-semibold bg-yellow-100 py-1 px-2 rounded-md text-xs">
@@ -18,7 +21,9 @@ export default function Product({ product }) {
         <p className="my-1 font-bold">UGX {product.currentPrice}</p>
         <div className="flex justify-between gap-4 items-center">
           <s className="text-sm">UGX {product.originalPrice}</s>
-          <p className="bg-orange-100 rounded-md p-1 ">-4.4%</p>
+          <p className="bg-orange-100 rounded-md p-1 ">
+            -{discount.toFixed(1)}%
+          </p>
         </div>
         <button className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">
           Buy Now
