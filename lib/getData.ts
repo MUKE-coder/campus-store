6cc4e9cba6ddcadee32d9dd207b03a087d805c75
kooza-colline <koozacollinz1@gmail.com>
2024-10-24 10:21:17 +0300

    debugging errors

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,12 +1,8 @@
-// Opt out of caching for all data requests in the route segment
-export const dynamic = "force-dynamic";
 export async function getData(endpoint:string) {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
     // console.log(`${baseUrl}/api/${endpoint}`);
-    const response = await fetch(`${baseUrl}/api/${endpoint}`, {
-      cache: "no-store",
-    });
+    const response = await fetch(`${baseUrl}/api/${endpoint}`);
     const data = await response.json();
     return data;
   } catch (error) {
